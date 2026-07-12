60086ef9e1480568485d3578857c629cc4ba34e5
ApraS231 <AlvinPradana0203@gmail.com>
2026-07-13 01:03:28 +0800

    add: temporary debug route

--- backend-api/routes/web.php
+++ backend-api/routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Route::get('/register-ortu', [RegistrationController::class, 'create'])->name('register.ortu');
 Route::post('/register-ortu', [RegistrationController::class, 'store'])->name('register.ortu.store');
 Route::get('/register-ortu/success', [RegistrationController::class, 'success'])->name('register.success');
+
+Route::get('/debug-log', function () {
+    $logPath = storage_path('logs/laravel.log');
+    if (!file_exists($logPath)) {
+        return "Log file not found.";
+    }
+    $lines = file($logPath);
+    $lastLines = array_slice($lines, -100);
+    return response(implode("", $lastLines), 200, ['Content-Type' => 'text/plain']);
+});
